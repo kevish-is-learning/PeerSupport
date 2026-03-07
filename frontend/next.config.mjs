557272d216477840a704387e3ee319cf83b88d5a
Kevish Sewliya <prokevish07@gmail.com>
2026-03-08 03:54:41 +0530

    Frontend Mapping

--- frontend/next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    domains: ['localhost', 'lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
-  },
-};
-
-export default nextConfig;
